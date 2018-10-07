9f7765bc559cec6ad7c0ef748a45e4f6155fd17d
Alex Cardelle <acardelle@icloud.com>
2018-10-07 12:06:43 -0400

    First functioning redirect with username

--- signup.py
+++ signup.py
@@ -6,9 +6,15 @@ app = Flask(__name__)
 app.config['DEBUG'] = True  
 
 @app.route("/")
-def index():
-    return render_template('home_form.html', title="Valimate")
+def display_signup_form():
+    return render_template('home_form.html', title="Welcome to Alexbook | Sign Up Page")
 
+@app.route("/", methods=['POST'])
+def validate_signup():
+  
+  username = request.form['usr_name']
+
+  return render_template('welcome_form.html', name=username)
 
 
 
