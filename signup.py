09441814b1d4aa6c5efae2164b5317b556b8271f
Alex Cardelle <acardelle@icloud.com>
2018-10-07 10:36:41 -0400

    First attempt at User-Signup

---
+++ signup.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, redirect, render_template
+import cgi
+import os
+
+app = Flask(__name__)
+app.config['DEBUG'] = True  
+
+@app.route("/")
+def index():
+    return render_template('home_form.html', title="Valimate")
+
+
+
+
+app.run()
